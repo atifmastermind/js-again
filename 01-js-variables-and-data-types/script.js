1c34cfbb4acbe415aa8e396ed770879a7a700d2b
Muhammad Atif <Atifmastermind12@gmail.com>
2024-08-27 19:15:43 +0500

    data types

--- 01-js-variables-and-data-types/script.js
+++ 01-js-variables-and-data-types/script.js
@@ -6,5 +6,141 @@
 // var variableName = "welcome to variables";
 // console.log(variableName);
 
-let variableName = "welcome to variables";
-console.log(variableName);
+// let variableName = "welcome to variables";
+// console.log(variableName);
+
+// ? DATA Types
+
+// ? Strings
+// Strings should be in text value.
+// ? single quotes string
+// let singleQuote = "my string";
+// console.log(singleQuote);
+// ? double quotes string
+// let doublequote = "my string";
+// console.log(doublequote);
+// ? backticks quotes string "its a complex string"
+// let backticksQuote = `my string backticks`;
+// console.log(backticksQuote);
+
+// example:-
+
+// let name = "Muhammad Atif";
+// let result = `Hello, ${name}, welcome to my JS`;
+// console.log(result);
+
+// ? typeof
+//  used this to know the type of the variable
+
+// In JavaScript, there are three ways to write a string — they can be written inside single quotes ( ' ' ), double quotes ( " " ), or backticks ( ` ` )
+
+// ?  Numbers
+// numbers should be in numbers values.
+
+// const wholeNumbers = 5;
+// const deimalNumbers = 0.5;
+// console.log(wholeNumbers);
+// console.log(deimalNumbers);
+
+// const firstName = 5;
+// const secondName = 10;
+// const thirdName = "Hellow";
+// ? addition
+// const result = firstName + secondName;
+// ? subtraction
+// const result = firstName - secondName;
+// ? multiplication
+// const result = firstName * secondName;
+// ? division
+// const result = firstName / secondName;
+
+// !when we use mathematical operation with string and numbers we get a MaN, its a incorrect opration.
+// const resultNew = thirdName / secondName;
+
+// console.log(resultNew);
+
+// ?  Booleans
+// a boolean value is one that can either be TRUE or FALSE.
+// true - yes, corrent ,1
+// false - no, incorrent ,0
+
+// ? Example:
+// const isCool = true;
+// if (isCool) {
+//   console.log("Hi men you're cool");
+// } else {
+//   console.log("hi men you're not cool at all");
+// }
+
+// ? Example 2:
+
+// const age = 30;
+// if (age > 18) {
+//   console.log("yes you are older then 18");
+// } else {
+//   console.log("your not elegible");
+// }
+
+// ?  Null
+//its empty at the moment and may have a value later.
+// has only one value, 1st we have null value then we can add a value to replace the null value.
+// ? example:
+// let age = null;
+// age = 20;
+// console.log(age);
+
+// ? Undefined
+// a variable that has not been assigned a value
+// ? example:
+
+// let x;
+// console.log(x);
+
+// ! NOTE: normaly we use null to assign empty or unknown value to a variable and we use undefined for checks like seeing if a variable has been assigned.
+
+// ? Object
+
+// object is a standalone entity, with properties and type.
+
+// objects are used to store collections of data in more complex entities,
+
+// let dataCollection = {
+//   name: "Muhammad Atif",
+//   Age: 30,
+// };
+
+// console.log(dataCollection.name);
+
+// ? arrays
+// to store order data collections
+
+// example:
+// const arr = [1,2,3,4,5]
+
+// 2 types of programming language
+
+// ? statically typed
+// static typing refers to a type system in which variable types are checked at compile-time. This means that variables must be declared with their intended type, ensuring that only compatible operations can be performed on them.
+
+// ? dynamically typed
+
+// programming languages that allow variables to change their identity at any time. In these languages, the interpreter assigns a type to a variable based on the variable's value at runtime.
+
+// ? Symbol
+
+// Primitive data types
+
+// Strings
+// Numbers
+// Booleans
+// Null
+// Undefined
+// Symbol
+
+// complex data types
+
+// Objects
+// Arrays,
+// Functions,
+// Maps,
+// Sets
