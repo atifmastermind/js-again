55c5d1c64b110a871109643a3308e4155e8ee2d3
Muhammad Atif <Atifmastermind12@gmail.com>
2024-08-27 14:23:04 +0500

    variables

---
+++ 01-js-variables-and-data-types/script.js
@@ -0,0 +1,10 @@
+// var "global old way"
+// let "scope variable"
+// const "it's can't be change when defined"
+
+// old way
+// var variableName = "welcome to variables";
+// console.log(variableName);
+
+let variableName = "welcome to variables";
+console.log(variableName);
