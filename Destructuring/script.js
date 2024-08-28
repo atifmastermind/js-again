b1adbe47b824cfd1c44fc558c1a815821181d26c
Muhammad Atif <Atifmastermind12@gmail.com>
2024-08-28 19:59:37 +0500

    destructuring

---
+++ Destructuring/script.js
@@ -0,0 +1,20 @@
+// ?  Array Destructuring:
+
+const vehicles = ["Swift", "Blackhawk", "Fortuner"];
+
+const [car, truck, suv] = vehicles;
+
+console.log("Car is", car);
+console.log("Truck is", truck);
+console.log("suv is", suv);
+
+// ? Object Destructuring:
+
+// let gfg = {
+//   fullName: "GeeksforGeeks",
+//   type: "eduTech",
+//   location: "Noida",
+// };
+
+// let { fullName, location: cLocation } = gfg;
+// console.log("company Name is " + fullName + " which is located in " + cLocation);
