7f8de7959bf0756fa5061d77d2ee4edc6374cfd0
Muhammad Atif <Atifmastermind12@gmail.com>
2024-08-28 13:11:21 +0500

    loops

---
+++ 03-js-loops/script.js
@@ -0,0 +1,28 @@
+// ? Example:
+// const age = 18;
+// if (age > age) {
+//   console.log("you may enter!");
+// } else if (age === 18) {
+//   console.log("you just turn 18, welcome");
+// } else {
+//   console.log("you not eligible sorry!! :( see you after you turn 18 ");
+// }
+
+// ? Example 2
+// ! While Loop
+// executes a block of code as long as a specified condition is true.
+
+// let i = 0;
+// while (i < 10) {
+//   console.log(i);
+//   i++;
+// }
+
+// ? Example 3
+
+// ! for Loop
+// A for loop repeats until a specified condition evaluates to false. The JavaScript for loop is similar to the Java and C for loop.
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
